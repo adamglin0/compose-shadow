a114992672cbee1f14523189dc90bd873ed27d7b
adamglin <dev@adamglin>
2024-09-22 16:17:30 +0800

    add androidx annotation

--- compose-shadow/src/androidMain/kotlin/com/adamglin/composeshadow/utils/maskFilter.android.kt
+++ compose-shadow/src/androidMain/kotlin/com/adamglin/composeshadow/utils/maskFilter.android.kt
@@ -1,8 +1,11 @@
 package com.adamglin.composeshadow.utils
 
 import android.graphics.BlurMaskFilter
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.ui.graphics.NativePaint
 
+@RequiresApi(Build.VERSION_CODES.P)
 internal actual fun NativePaint.setMaskFilter(blurRadius: Float) {
     maskFilter = BlurMaskFilter(blurRadius, android.graphics.BlurMaskFilter.Blur.NORMAL)
 }
