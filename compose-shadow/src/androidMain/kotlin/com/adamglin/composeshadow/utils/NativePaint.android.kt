abcf68021fe395ffb618860be254c28d852752bb
Houssam Elbadissi <skaldebane@gmail.com>
2024-11-13 10:28:31 +0100

    Merge Skiko targets, simplify code

--- compose-shadow/src/androidMain/kotlin/com/adamglin/composeshadow/utils/NativePaint.android.kt
+++ compose-shadow/src/androidMain/kotlin/com/adamglin/composeshadow/utils/NativePaint.android.kt
@@ -1,6 +1,7 @@
 package com.adamglin.composeshadow.utils
 
 import android.graphics.BlurMaskFilter
+import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
 import android.os.Build
 import androidx.annotation.RequiresApi
@@ -14,4 +15,28 @@ internal actual fun NativePaint.setMaskFilter(blurRadius: Float) {
 
 internal actual fun NativePaint.setBlendMode(blendMode: BlendMode) {
     xfermode = PorterDuffXfermode(blendMode.toPorterDuffMode())
+}
+
+private fun BlendMode.toPorterDuffMode(): PorterDuff.Mode {
+    return when (this) {
+        BlendMode.Clear -> PorterDuff.Mode.CLEAR
+        BlendMode.Src -> PorterDuff.Mode.SRC
+        BlendMode.Dst -> PorterDuff.Mode.DST
+        BlendMode.SrcOver -> PorterDuff.Mode.SRC_OVER
+        BlendMode.DstOver -> PorterDuff.Mode.DST_OVER
+        BlendMode.SrcIn -> PorterDuff.Mode.SRC_IN
+        BlendMode.DstIn -> PorterDuff.Mode.DST_IN
+        BlendMode.SrcOut -> PorterDuff.Mode.SRC_OUT
+        BlendMode.DstOut -> PorterDuff.Mode.DST_OUT
+        BlendMode.SrcAtop -> PorterDuff.Mode.SRC_ATOP
+        BlendMode.DstAtop -> PorterDuff.Mode.DST_ATOP
+        BlendMode.Xor -> PorterDuff.Mode.XOR
+        BlendMode.Darken -> PorterDuff.Mode.DARKEN
+        BlendMode.Lighten -> PorterDuff.Mode.LIGHTEN
+        BlendMode.Multiply -> PorterDuff.Mode.MULTIPLY
+        BlendMode.Screen -> PorterDuff.Mode.SCREEN
+        BlendMode.Plus -> PorterDuff.Mode.ADD
+        BlendMode.Overlay -> PorterDuff.Mode.OVERLAY
+        else -> throw IllegalArgumentException("Not Allow convert BlendMode to PorterDuffMode")
+    }
 }
