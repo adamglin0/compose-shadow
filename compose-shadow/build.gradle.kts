896ca1fa843e7420d9d4d213fff5ba544b75369c
Adamglin <149357973+adamglin0@users.noreply.github.com>
2024-12-02 20:23:48 +0800

    release/2.0.1 (#41)

--- compose-shadow/build.gradle.kts
+++ compose-shadow/build.gradle.kts
@@ -97,7 +97,7 @@ mavenPublishing {
     coordinates(
         groupId = "com.adamglin",
         artifactId = "compose-shadow",
-        version = "2.0.0"
+        version = "2.0.1"
     )
     pom {
         name.set("compose-shadow")
