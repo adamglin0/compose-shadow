21ab303c26c3ce76dc49aee245d01f3897cac4e1
adamglin0 <149357973+adamglin0@users.noreply.github.com>
2025-02-16 14:09:12 +0800

    fix(sample-app): can not find coroutine-main in desktop (#45)

--- sample/compose-app/src/desktopMain/kotlin/com/adamglin/composeshadow/app/main.kt
+++ sample/compose-app/src/desktopMain/kotlin/com/adamglin/composeshadow/app/main.kt
@@ -6,11 +6,17 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import androidx.compose.ui.window.singleWindowApplication
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 fun main() {
     application {
         val windowState = rememberWindowState(width = 360.dp, height = 700.dp)
         Window(state = windowState, onCloseRequest = ::exitApplication) {
+            GlobalScope.launch(Dispatchers.Main.immediate) {
+                println("hello")
+            }
             SampleApp()
         }
     }
